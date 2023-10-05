ca958b445eb49e9db9df4781e2dbb57181404afc
pitzee <pitzee421@gmail.com>
2023-10-05 19:55:17 +0000

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!usr/bin/env python3
-#Basic annotations - add
+""" Basic annotations - add """
+
 
 def add(a: float, b:float) -> float:
-    #return sum of a and b
+    """ return sum of a and b """
     return a + b
