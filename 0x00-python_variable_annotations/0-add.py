9ea84a4a61bb844c8d0404355af0a7337b0af07d
pitzee <pitzee421@gmail.com>
2023-10-05 20:09:34 +0000

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
-#!usr/bin/env python3
-""" Basic annotations - add """
+#!/usr/bin/env python3
+"""Module that contains a single function that adds two integers."""
 
 
 def add(a: float, b: float) -> float:
-    """ return sum of a and b """
+    """ Add two integers together."""
     return a + b
